d8df4ed41969922a44e265bea7378be6ca487c9d
rafflezs <thi-rafael@hotmail.com.br>
2022-05-19 18:12:18 -0300

    adcionado classe professor

---
+++ src/cpp/classes/professor.hpp
@@ -0,0 +1,24 @@
+#include <string>
+
+class Professor
+{
+private:
+  string nome;
+  string id;
+  int pos_id;
+
+public:
+
+  nome
+
+  Professor(/* args */);
+  ~Professor();
+};
+
+Professor::Professor(/* args */)
+{
+}
+
+Professor::~Professor()
+{
+}
