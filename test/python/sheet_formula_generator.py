9fea9c823cae4e0e2daf25fc773b9e8c512c7a7d
Rafflezs <thi-rafael@gmail.com.br>
2022-08-29 20:23:54 -0300

    Instncias finalizadas

--- test/python/sheet_formula_generator.py
+++
@@ -1,3 +0,0 @@
-with open('./data/automated_data/disciplina_turno_formula.txt', 'w+') as file:
-  for i in range (2,105):
-    file.write('=ÍNDICE(Turmas!D2:D104;CORRESP(E'+str(i)+';Turmas!A2:A104;0))\n')
