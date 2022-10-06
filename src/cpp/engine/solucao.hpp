1d17a430050871c1694cb11e0b95225a44b8f9e3
Rafflezs <thi-rafael@hotmail.com.br>
2022-10-06 07:49:14 -0300

    TODO: le a porra do solucao.hpp seu burro

--- src/cpp/engine/solucao.hpp
+++ src/cpp/engine/solucao.hpp
@@ -3,6 +3,20 @@
 
 class Solucao {
 
+// ! TODO: Implementar Inicialização da Solução -> Solução Zero
+// ! TODO: Implementar Herística Construtiva da Solução -> Inserir Inicio
+    // ! Verificar ordenação utilizada na Heurística Construtiva
+// ! TODO: Implementar Impressão da Solução
+
+
+// Instancia - Computação
+// Instancia (hard) - Biologias e Biotecnologia
+
+// TODO: Implementar leitura da Solução
+// TODO: Implementar exclusão da Solução
+// TODO: Implementar inserção da Solução
+
+
 };
 
 #endif //!_SOLUCAO_HPP
