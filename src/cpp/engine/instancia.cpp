de335c8c54593e0b9f2c953ca02b8a7488e558ed
rafflezs <thi-rafael@hotmail.com.br>
2022-10-20 15:15:00 -0300

    fix: ponteiros das primitivas em instancias em enderecos diferentes

--- src/cpp/engine/instancia.cpp
+++ src/cpp/engine/instancia.cpp
@@ -10,16 +10,16 @@ Instancia::Instancia(const std::string ano_instancia)
     this->m_lista_turmas = instanciar_turma(ano_instancia);
     std::cout << "Gerou lista de turmas" << std::endl;
 
-    // this->m_lista_professores = instanciar_professor(ano_instancia);
-    // std::cout << "Gerou lista de professores" << std::endl;
+    this->m_lista_professores = instanciar_professor(ano_instancia);
+    std::cout << "Gerou lista de professores" << std::endl;
 
-    // this->m_lista_cursos = instanciar_curso(ano_instancia);
-    // std::cout << "Gerou lista de cursos" << std::endl;
+    this->m_lista_cursos = instanciar_curso(ano_instancia);
+    std::cout << "Gerou lista de cursos" << std::endl;
 }
 
-const std::vector<Professor> Instancia::instanciar_professor(const std::string &ano_instancia)
+const std::vector<Professor *> Instancia::instanciar_professor(const std::string &ano_instancia)
 {
-    std::vector<Professor> t_professores;
+    std::vector<Professor *> t_professores;
     std::ifstream file{"/home/rafflezs/TCC/data/csv/" + ano_instancia + "/Professores.csv"}; // WSL
     // std::ifstream file{"/home/rafflezs/Programacao/TCC/data/csv/" + ano_instancia + "/Professores.csv"}; // Windows
     // std::ifstream file{"D:\\Programacao\\TCC\\data\\csv\\" + ano_instancia + "\\Professores.csv"}; // Unix
@@ -30,31 +30,12 @@ const std::vector<Professor> Instancia::instanciar_professor(const std::string &
     {
         if (index > 0)
         {
-
-            std::vector<Disciplina *> t_disc{nullptr};
-
-            auto nome_disciplinas = m_csv.parse_coluna((*row)[2], ',');
-
-            for (auto parse_disciplina : nome_disciplinas)
-            {
-                for (auto it : m_lista_disciplinas)
-                {
-                    if (parse_disciplina == it.get_id())
-                    {
-                        // std::cout << "Disciplina: " << it.get_nome() << " | Endereço -> " << &it << " | ";
-                        t_disc.push_back(&it);
-                        continue;
-                    }
-                }
-            }
-
-            t_professores.push_back(
-                Professor(
-                    (*row)[0],
-                    index,
-                    (*row)[1],
-                    t_disc,
-                    m_csv.preencher_disponibilidade({(*row)[3], (*row)[4], (*row)[5], (*row)[6], (*row)[7], (*row)[8]})));
+            t_professores.push_back(new Professor(
+                (*row)[0],
+                index,
+                (*row)[1],
+                buscar_disciplinas(m_csv.parse_coluna((*row)[2], ',')),
+                m_csv.preencher_disponibilidade({(*row)[3], (*row)[4], (*row)[5], (*row)[6], (*row)[7], (*row)[8]})));
         }
         index++;
     }
@@ -62,9 +43,9 @@ const std::vector<Professor> Instancia::instanciar_professor(const std::string &
     return t_professores;
 }
 
-const std::vector<Curso> Instancia::instanciar_curso(const std::string &ano_instancia)
+const std::vector<Curso *> Instancia::instanciar_curso(const std::string &ano_instancia)
 {
-    std::vector<Curso> t_cursos;
+    std::vector<Curso *> t_cursos;
     std::ifstream file{"/home/rafflezs/TCC/data/csv/" + ano_instancia + "/Cursos.csv"}; // WSL
     // std::ifstream file{"/home/rafflezs/Programacao/TCC/data/csv/" + ano_instancia + "/Cursos.csv"}; // Windows
     // std::ifstream file{"D:\\Programacao\\TCC\\data\\csv\\" + ano_instancia + "\\Cursos.csv"}; // Unix
@@ -76,12 +57,12 @@ const std::vector<Curso> Instancia::instanciar_curso(const std::string &ano_inst
         if (index > 0)
         {
             t_cursos.push_back(
-                Curso(
+                new Curso(
                     (*row)[0],
                     index,
                     (*row)[1],
                     (*row)[2],
-                    {},
+                    buscar_turmas(m_csv.parse_coluna((*row)[3], ',')),
                     m_csv.preencher_disponibilidade({(*row)[4], (*row)[5], (*row)[6], (*row)[7], (*row)[8], (*row)[9]})));
         }
         index++;
@@ -90,9 +71,9 @@ const std::vector<Curso> Instancia::instanciar_curso(const std::string &ano_inst
     return t_cursos;
 }
 
-const std::vector<Turma> Instancia::instanciar_turma(const std::string &ano_instancia)
+const std::vector<Turma *> Instancia::instanciar_turma(const std::string &ano_instancia)
 {
-    std::vector<Turma> t_turmas;
+    std::vector<Turma *> t_turmas;
     std::ifstream file{"/home/rafflezs/TCC/data/csv/" + ano_instancia + "/Turmas.csv"}; // WSL
     // std::ifstream file{"/home/rafflezs/Programacao/TCC/data/csv/" + ano_instancia + "/Turmas.csv"}; // Windows
     // std::ifstream file{"D:\\Programacao\\TCC\\data\\csv\\" + ano_instancia + "\\Turmas.csv"}; // Unix
@@ -103,32 +84,13 @@ const std::vector<Turma> Instancia::instanciar_turma(const std::string &ano_inst
     {
         if (index > 0)
         {
-
-            std::vector<Disciplina *> t_disc{nullptr};
-
-            auto nome_disciplinas = m_csv.parse_coluna((*row)[4], ',');
-
-            for (auto parse_disciplina : nome_disciplinas)
-            {
-                for (auto it : m_lista_disciplinas)
-                {
-                    if (parse_disciplina == it.get_id())
-                    {
-                        // std::cout << "Disciplina: " << it.get_nome() << " | Endereço -> " << &it << " | ";
-                        t_disc.push_back(&it);
-                        continue;
-                    }
-                }
-            }
-
-            t_turmas.push_back(
-                Turma(
-                    (*row)[0],
-                    index,
-                    (*row)[2],
-                    (*row)[3],
-                    t_disc,
-                    m_csv.preencher_disponibilidade({(*row)[5], (*row)[6], (*row)[7], (*row)[8], (*row)[9], (*row)[10]})));
+            t_turmas.push_back(new Turma(
+                (*row)[0],
+                index,
+                (*row)[2],
+                (*row)[3],
+                buscar_disciplinas(m_csv.parse_coluna((*row)[4], ',')),
+                m_csv.preencher_disponibilidade({(*row)[5], (*row)[6], (*row)[7], (*row)[8], (*row)[9], (*row)[10]})));
         }
         index++;
     }
@@ -136,9 +98,9 @@ const std::vector<Turma> Instancia::instanciar_turma(const std::string &ano_inst
     return t_turmas;
 }
 
-const std::vector<Disciplina> Instancia::instanciar_disciplina(const std::string &ano_instancia)
+const std::vector<Disciplina *> Instancia::instanciar_disciplina(const std::string &ano_instancia)
 {
-    std::vector<Disciplina> t_disciplinas;
+    std::vector<Disciplina *> t_disciplinas;
     std::ifstream file{"/home/rafflezs/TCC/data/csv/" + ano_instancia + "/Disciplinas.csv"}; // WSL
     // std::ifstream file{"/home/rafflezs/Programacao/TCC/data/csv/" + ano_instancia + "/Disciplinas.csv"}; // Windows
     // std::ifstream file{"D:\\Programacao\\TCC\\data\\csv\\" + ano_instancia + "\\Disciplinas.csv"}; // Unix
@@ -149,7 +111,7 @@ const std::vector<Disciplina> Instancia::instanciar_disciplina(const std::string
     {
         if (index > 0)
         {
-            t_disciplinas.push_back(Disciplina(
+            t_disciplinas.push_back(new Disciplina(
                 (*row)[0],
                 (*row)[1],
                 index,
@@ -175,10 +137,10 @@ std::vector<Disciplina *> Instancia::buscar_disciplinas(const std::vector<std::s
     {
         for (auto it : m_lista_disciplinas)
         {
-            if (parse_disciplina == it.get_id())
+            if (parse_disciplina == it->get_id())
             {
-                // std::cout << "Disciplina: " << it.get_nome() << " | Endereço -> " << &it << " | ";
-                t_disc.push_back(&it);
+                // std::cout << "Disciplina: " << it->get_nome() << " | Endereço -> " << &it << " | ";
+                t_disc.push_back(it);
                 continue;
             }
         }
@@ -187,4 +149,54 @@ std::vector<Disciplina *> Instancia::buscar_disciplinas(const std::vector<std::s
     // std::cout << "\n" << std::endl;
 
     return t_disc;
+}
+
+std::vector<Turma *> Instancia::buscar_turmas(const std::vector<std::string> nome_turmas)
+{
+
+    std::vector<Turma *> t_turma{nullptr};
+
+    for (auto parse_disciplina : nome_turmas)
+    {
+        for (auto it : m_lista_turmas)
+        {
+            if (parse_disciplina == it->get_id())
+            {
+                // std::cout << "Turma: " << it->get_nome() << " | Endereço -> " << &it << " | ";
+                t_turma.push_back(it);
+                continue;
+            }
+        }
+    }
+
+    // std::cout << "\n" << std::endl;
+
+    return t_turma;
+}
+
+void Instancia::print_instancia()
+{
+    std::cout << "\n\n--------Lista de Disciplinas--------\n";
+    for (auto it = m_lista_disciplinas.begin(); it < m_lista_disciplinas.end(); it++)
+    {
+        (*it)->print();
+    }
+
+    std::cout << "\n\n--------Lista de Cursos--------\n";
+    for (auto it = m_lista_cursos.begin(); it < m_lista_cursos.end(); it++)
+    {
+        (*it)->print();
+    }
+
+    std::cout << "\n\n--------Lista de Professores--------\n";
+    for (auto it = m_lista_professores.begin(); it < m_lista_professores.end(); it++)
+    {
+        (*it)->print();
+    }
+
+    std::cout << "\n\n--------Lista de Turmas--------\n";
+    for (auto it = m_lista_turmas.begin(); it < m_lista_turmas.end(); it++)
+    {
+        (*it)->print();
+    }
 }
--- src/cpp/engine/instancia.hpp
+++ src/cpp/engine/instancia.hpp
@@ -15,17 +15,20 @@ private:
 public:
     Instancia(const std::string ano_instancia);
 
-    std::vector<Turma> m_lista_turmas;
-    std::vector<Professor> m_lista_professores;
-    std::vector<Disciplina> m_lista_disciplinas;
-    std::vector<Curso> m_lista_cursos;
+    std::vector<Turma*> m_lista_turmas;
+    std::vector<Professor*> m_lista_professores;
+    std::vector<Disciplina*> m_lista_disciplinas;
+    std::vector<Curso*> m_lista_cursos;
 
-    const std::vector<Professor> instanciar_professor(const std::string &ano_instancia);
-    const std::vector<Curso> instanciar_curso(const std::string &ano_instancia);
-    const std::vector<Turma> instanciar_turma(const std::string &ano_instancia);
-    const std::vector<Disciplina> instanciar_disciplina(const std::string &ano_instancia);
+    const std::vector<Professor*> instanciar_professor(const std::string &ano_instancia);
+    const std::vector<Curso*> instanciar_curso(const std::string &ano_instancia);
+    const std::vector<Turma*> instanciar_turma(const std::string &ano_instancia);
+    const std::vector<Disciplina*> instanciar_disciplina(const std::string &ano_instancia);
 
     std::vector <Disciplina*> buscar_disciplinas(const std::vector<std::string> nome_disciplinas);
+    std::vector <Turma*> buscar_turmas(const std::vector<std::string> nome_turmas);
+
+    void print_instancia();
 };
 
 #endif //!_INSTNACIA_HPP
