64e0e93470468b391d4cabba30233af79c25b82a
rafflezs <thi-rafael@hotmail.com.br>
2023-06-19 11:45:08 -0300

    validator com repeticao de execucoes

--- test/validator.sh
+++ test/validator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ulimit -v 8000000  # Setar limite para execucao
+ulimit -v 16000000  # Setar limite para execucao
 
 # Fase de compilacao
 if [ -f "./test/exe" ]; then
@@ -16,10 +16,8 @@ if [ -f "./test/exe" ]; then
     echo ""
 fi
 
-# Limpando pasta output/
-rm -r data/output/*
-
-# Lista de parametros
+# Definir quantidade de execuções
+QTD_EXEC=5
 INSTANCIAS_LIST=("TCC-Instancia-2018-1" "TCC-Instancia-2018-2" "TCC-Instancia-2019-1" "TCC-Instancia-2019-2") # "TCC-Instancia-2022-1")
 REPTS_LIST=(1 2 5 10)
 QTD_TURMAS_LIST=(0 1 2 1000)
@@ -27,89 +25,105 @@ PESOS_JANELA_LIST=(1 2 5)
 PESOS_SEXTO_LIST=(1 2 5)
 TAM_POPULACAO=50
 
-# #
-# # Main drive: loop principal de parametros
-# #
+# Loop externo
+for ((execucao = 1; execucao <= QTD_EXEC; execucao++)); do
 
-# Variavel the progresso para saber quanto falta pra acabar.
-total=$((${#INSTANCIAS_LIST[@]} * ${#REPTS_LIST[@]} * ${#QTD_TURMAS_LIST[@]} * ${#PESOS_JANELA_LIST[@]} * ${#PESOS_SEXTO_LIST[@]}))
-progresso_atual=0
-erros=0
-
-# Cria arquivo para acompanhar exec_time
-echo "ITERACAO;INSTANCIA_NOME;TAM_POPULACAO;QTD_TURMAS_HEURISTICA;QTD_REPT_HEURISTICA;PESO_JANELA;PESO_SEXTO;TEMPO_EXEC;ID_SOLUCAO;JANELA_SOLUCAO;SEXTO_HORARIO_SOLUCAO;VALOR_SOLUCAO" >> "data/bash-time.csv"
-
-for instancia_value in "${INSTANCIAS_LIST[@]}"; do
-    output_dir="data/output/$instancia_value"
+    # Cria pasta para a iteração
+    output_dir="data/output/Execucao${execucao}"
     mkdir -p "$output_dir"
-    echo "Instancia: $instancia_value"
 
-    for qtd_turma_value in "${QTD_TURMAS_LIST[@]}"; do
-        for qtd_rept_value in "${REPTS_LIST[@]}"; do
-            for peso_janela_value in "${PESOS_JANELA_LIST[@]}"; do
-                for peso_sexto_value in "${PESOS_SEXTO_LIST[@]}"; do
+    # Limpando pasta output/
+    rm -r "$output_dir"/*
 
-                    # Cria subpasta da nova execucao
-                    subfolder_name="qtdTurmas${qtd_turma_value}-REPTS_LIST${qtd_rept_value}-Jan${peso_janela_value}-Sex${peso_sexto_value}"
-                    subfolder_path="$output_dir/$subfolder_name"
-                    mkdir -p "$subfolder_path"
+    # Lista de parametros
 
-                    echo "qtd turmas heuristica: $qtd_turma_value"
-                    echo "qtd repet heuristica: $qtd_rept_value"
-                    echo "peso janela: $peso_janela_value"
-                    echo "peso sexto: $peso_sexto_value"
-                    echo ""
 
-                    # Checa se houve runtime error
-                    if { time -p (./test/exe "$instancia_value" "$TAM_POPULACAO" "$qtd_turma_value" "$qtd_rept_value" "$peso_janela_value" "$peso_sexto_value") ; } 2> >(grep real | awk '{print $2}' > "data/output/time.txt") > /dev/null ; then
-                        echo "Tempo de execucao: $exec_time segundos."
+    # #
+    # # Main drive: loop principal de parametros
+    # #
+
+    # Variavel the progresso para saber quanto falta pra acabar.
+    total=$((${#INSTANCIAS_LIST[@]} * ${#REPTS_LIST[@]} * ${#QTD_TURMAS_LIST[@]} * ${#PESOS_JANELA_LIST[@]} * ${#PESOS_SEXTO_LIST[@]}))
+    progresso_atual=0
+    erros=0
+
+    # Cria arquivo para acompanhar exec_time
+    echo "ITERACAO;INSTANCIA_NOME;TAM_POPULACAO;QTD_TURMAS_HEURISTICA;QTD_REPT_HEURISTICA;PESO_JANELA;PESO_SEXTO;TEMPO_EXEC;ID_SOLUCAO;JANELA_SOLUCAO;SEXTO_HORARIO_SOLUCAO;VALOR_SOLUCAO" >> "$output_dir/bash-time.csv"
+
+    for instancia_value in "${INSTANCIAS_LIST[@]}"; do
+        output_dir_iteration="$output_dir/$instancia_value"
+        mkdir -p "$output_dir_iteration"
+        echo "Instancia: $instancia_value"
+
+        for qtd_turma_value in "${QTD_TURMAS_LIST[@]}"; do
+            for qtd_rept_value in "${REPTS_LIST[@]}"; do
+                for peso_janela_value in "${PESOS_JANELA_LIST[@]}"; do
+                    for peso_sexto_value in "${PESOS_SEXTO_LIST[@]}"; do
+
+                        # Cria subpasta da nova execucao
+                        subfolder_name="qtdTurmas${qtd_turma_value}-REPTS_LIST${qtd_rept_value}-Jan${peso_janela_value}-Sex${peso_sexto_value}"
+                        subfolder_path="$output_dir_iteration/$subfolder_name"
+                        mkdir -p "$subfolder_path"
+
+                        echo "qtd turmas heuristica: $qtd_turma_value"
+                        echo "qtd repet heuristica: $qtd_rept_value"
+                        echo "peso janela: $peso_janela_value"
+                        echo "peso sexto: $peso_sexto_value"
                         echo ""
 
-                        # Read values from pequena_trollagem.txt
-                        IFS=';' read -r best_id best_window_value best_sixth_grade_value solution_value < "data/pequena_trollagem.txt"
+                        # Checa se houve runtime error
+                        if { time -p (./test/exe "$instancia_value" "$TAM_POPULACAO" "$qtd_turma_value" "$qtd_rept_value" "$peso_janela_value" "$peso_sexto_value") ; } 2> >(grep real | awk '{print $2}' > "$output_dir_iteration/time.txt") > /dev/null ; then
+                            echo "Tempo de execucao: $exec_time segundos."
+                            echo ""
 
-                        # Move arquivos para subpasta criada
-                        echo "Movendo arquivos."
-                        if [ -f "data/output/debug-out.txt" ]; then
-                            mv "data/output/debug-out.txt" "$subfolder_path/"
+                            # Read values from pequena_trollagem.txt
+                            IFS=';' read -r best_id best_window_value best_sixth_grade_value solution_value < "data/pequena_trollagem.txt"
+
+                            # Move arquivos para subpasta criada
+                            echo "Movendo arquivos."
+                            if [ -f "data/output/debug-out.txt" ]; then
+                                mv "data/output/debug-out.txt" "$subfolder_path/"
+                            fi
+                            if [ -f "data/output/analise.csv" ]; then
+                                mv "data/output/analise.csv" "$subfolder_path/"
+                            fi
+
+                            # Executa o compilador LaTeX
+                            echo "Compilando LaTeX"
+                            if [ -f "data/output/professor.tex" ]; then
+                                mv "data/output/professor.tex" "$subfolder_path/"
+                                (cd "$subfolder_path" && pdflatex -halt-on-error -interaction=batchmode -output-directory . professor.tex > /dev/null)
+                            fi
+                            if [ -f "data/output/turma.tex" ]; then
+                                mv "data/output/turma.tex" "$subfolder_path/"
+                                (cd "$subfolder_path" && pdflatex -halt-on-error -interaction=batchmode -output-directory . turma.tex > /dev/null)
+                            fi
+
+                            ((progresso_atual++))
+                            echo "Iteracao completa: passo $progresso_atual de $total."
+                            echo "----------------------------------------------"
+                            echo ""
+
+                            exec_time=$(cat "$output_dir_iteration/time.txt")
+                            echo "$progresso_atual;$instancia_value;$TAM_POPULACAO;$qtd_turma_value;$qtd_rept_value;$peso_janela_value;$peso_sexto_value;$exec_time;$best_id;$best_window_value;$best_sixth_grade_value;$solution_value" >> "$output_dir_iteration/bash-time.csv"
+                        else
+                            ((progresso_atual++))
+                            echo "Falha na execucao: passo $progresso_atual de $total."
+                            echo "Erros encontrados: $erros"
+                            ((erros++))
+
+                            exec_time=$(cat "$output_dir_iteration/time.txt")
+                            echo "$progresso_atual;$instancia_value;$TAM_POPULACAO;$qtd_turma_value;$qtd_rept_value;$peso_janela_value;$peso_sexto_value;RUNTIME_ERROR;RUNTIME_ERROR;RUNTIME_ERROR;RUNTIME_ERROR" >> "$output_dir_iteration/bash-time.csv"
                         fi
-                        if [ -f "data/output/analise.csv" ]; then
-                            mv "data/output/analise.csv" "$subfolder_path/"
-                        fi
-
-                        # Executa o compilador LaTeX
-                        echo "Compilando LaTeX"
-                        if [ -f "data/output/professor.tex" ]; then
-                            mv "data/output/professor.tex" "$subfolder_path/"
-                            (cd "$subfolder_path" && pdflatex -halt-on-error -interaction=batchmode -output-directory . professor.tex > /dev/null)
-                        fi
-                        if [ -f "data/output/turma.tex" ]; then
-                            mv "data/output/turma.tex" "$subfolder_path/"
-                            (cd "$subfolder_path" && pdflatex -halt-on-error -interaction=batchmode -output-directory . turma.tex > /dev/null)
-                        fi
-
-                        ((progresso_atual++))
-                        echo "Iteracao completa: passo $progresso_atual de $total."
-                        echo "----------------------------------------------"
-                        echo ""
-
-                        exec_time=$(cat "data/output/time.txt")
-                        echo "$progresso_atual;$instancia_value;$TAM_POPULACAO;$qtd_turma_value;$qtd_rept_value;$peso_janela_value;$peso_sexto_value;$exec_time;$best_id;$best_window_value;$best_sixth_grade_value;$solution_value" >> "data/bash-time.csv"
-                    else
-                        ((progresso_atual++))
-                        echo "Falha na execucao: passo $progresso_atual de $total."
-                        echo "Erros encontrados: $erros"
-                        ((erros++))
-
-                        exec_time=$(cat "data/output/time.txt")
-                        echo "$progresso_atual;$instancia_value;$TAM_POPULACAO;$qtd_turma_value;$qtd_rept_value;$peso_janela_value;$peso_sexto_value;RUNTIME_ERROR;RUNTIME_ERROR;RUNTIME_ERROR;RUNTIME_ERROR" >> "data/bash-time.csv"
-                    fi
+                    done
                 done
             done
         done
     done
-done
 
-mv "data/bash-time.csv" "data/output/"
-echo ""
-echo "Programa encerrado com $erros! Resultados salvos em /data/output/"
+    mv "$output_dir_iteration/bash-time.csv" "$output_dir/"
+    echo ""
+    echo "Programa encerrado com $erros! Resultados salvos em $output_dir/"
+    echo ""
+
+done
